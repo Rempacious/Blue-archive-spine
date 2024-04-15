4da1ea68371bb604fce39bd4bd1ebad24d073ab8
Rempacious <73411987+Rempacious@users.noreply.github.com>
2024-04-15 19:47:30 +0800

    Update getModelsJapan.py
    JP Update

--- py/getModelsJapan.py
+++ py/getModelsJapan.py
@@ -11,7 +11,7 @@ option = {
     "skipExistingAssets": True
 }
 
-ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r66_e30ihsl382v0a1r8itw3_2.json"
+ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r66_byln9y195x3fefcjqjf5.json"
 
 ba_api2 = "https://prod-noticeindex.bluearchiveyostar.com/prod/index.json"
 
